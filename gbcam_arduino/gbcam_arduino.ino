ae9764bc5bf919c0b8d6975f22742030911d41b0
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2022-03-07 22:17:34 +0100

    Add files via upload

--- gbcam_arduino/gbcam_arduino.ino
+++ gbcam_arduino/gbcam_arduino.ino
@@ -1,53 +1,44 @@
-//Laurent Saint-Marcel (lstmarcel@yahoo.fr) 2005/07/05
-//Simplified and cleaned by Raphaël BOICHOT 2020/03/04
+//Simplified and completely rewritten by Raphaël BOICHOT in 2020/03/07 to be Arduino/ESP8266/ESP32 compilable 
+//from a code of Laurent Saint-Marcel (lstmarcel@yahoo.fr) 2005/07/05
 /*
-RESET/XRST has to be low on the rising edge of XCK
+RESET has to be low on the rising edge of CLOCK
 Raise LOAD high as you clear the last bit of each register you send
-START has to be high before rixing XCK
+START has to be high before rixing CLOCK
 Send START once
 The camera won't pulse the START pin; the datasheet is confusing about this
-READ goes high on rising XCK
-Read VOUT analog values shortly after you set XCK low
+READ goes high on rising CLOCK
+Read VOUT analog values shortly after you set CLOCK low
 */
-#include <compat/deprecated.h>
 
-#define CAM_DATA_PORT     PORTB
-#define CAM_DATA_DDR      DDRB
-#define CAM_READ_PIN      8       // Arduino D8
-#define CAM_LED_DDR       DDRB
-#define CAM_LED_PORT      PORTB
+int VOUT = A3; //Analogic-digital converter
+int READ = 8;
+int CLOCK = 9;
+int RESET = 10;
+int LOAD = 11;
+int SIN = 12;
+int START = 13;
+int LED = 4;
 
-// CAM_DATA_PORT
-#define CAM_START_BIT 5
-#define CAM_SIN_BIT   4
-#define CAM_LOAD_BIT  3
-#define CAM_RESET_BIT 2
-#define CAM_CLOCK_BIT 1
-#define CAM_READ_BIT  0
-
-// PORT C:
-#define CAM_ADC_PIN   A3 //Analogic/digital converter
-
-unsigned char camReg[8] = {155,0,0,0,1,0,1,7}; //default value for warm-up
-unsigned char camClockSpeed = 0x07; // was 0x0A
-boolean dataReady;
+unsigned char camReg[8] = {155,0,0,0,1,0,1,7}; //default value just for warm-up
 unsigned char reg;
 
 //main code//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup()
 {
-  dataReady = false;
   Serial.begin(115200);
   Serial.println("Camera initialisation");
-  pinMode(8, INPUT);   // READ
-  pinMode(9, OUTPUT);  // XCK
-  pinMode(10, OUTPUT);  // XRST
-  pinMode(11, OUTPUT); // LOAD
-  pinMode(12, OUTPUT); // SIN
-  pinMode(13, OUTPUT); // START
-  pinMode(4, OUTPUT);   // LED
+  pinMode(READ, INPUT);   // READ
+  pinMode(CLOCK, OUTPUT);  // CLOCK
+  pinMode(RESET, OUTPUT);  // RESET
+  pinMode(LOAD, OUTPUT); // LOAD
+  pinMode(SIN, OUTPUT); // SIN
+  pinMode(START, OUTPUT); // START
+  pinMode(LED, OUTPUT);   // LED
   camInit();
-  sei();// enable interrupts
+  for (int i = 0; i < 100; i++) {
+  Serial.println("Camera initialised"); //Octave is not happy without spamming the serial initially...
+  }
+  
 }
 
 void loop()//the main loop reads 8 registers from GNU Octave and send an image to serial
@@ -55,11 +46,12 @@ void loop()//the main loop reads 8 registers from GNU Octave and send an image t
 
   Serial.println("Ready for injecting registers");
   if (Serial.available() > 0) {
-    digitalWrite(4, HIGH);
+    digitalWrite(LED, HIGH);
     for (reg = 0; reg < 8; ++reg) {
     camReg[reg] = Serial.read(); //read the 8 current registers sent by GNU Octave
     }
-    digitalWrite(4, LOW);
+    delay(100);
+    digitalWrite(LED, LOW);
 
     Serial.print("Registers injected in sensor by GNU Octave for verification: ");
     for (reg = 0; reg < 8; ++reg) {
@@ -77,71 +69,39 @@ void loop()//the main loop reads 8 registers from GNU Octave and send an image t
     Serial.println("sensor reset");
 }
 
-} //main code//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+} //end of main code//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // PRIVATE FUNCTIONS
-
-// CAM TIMING AND CONTROL
-// cbi(port, bitId) = clear bit(port, bitId) = Set the signal Low
-// sbi(port, bitId) = set bit(port, bitId) = Set the signal High
-// Delay used between each signal sent to the AR (four per xck cycle).
-void camStepDelay() {
-  unsigned char u = camClockSpeed;
-  while (u--) {
-    __asm__ __volatile__ ("nop");
-  }
-}
-// Set the clock signal Low
-inline void camClockL()
-{
-  cbi(CAM_DATA_PORT, CAM_CLOCK_BIT);
-}
-// Set the clock signal High
-inline void camClockH()
-{
-  sbi(CAM_DATA_PORT, CAM_CLOCK_BIT);
-}
 
 // Initialise the IO ports for the camera
 void camInit()
 {
-  cbi(CAM_DATA_PORT, CAM_CLOCK_BIT);
-  sbi(CAM_DATA_PORT, CAM_RESET_BIT);  // Reset is active low
-  cbi(CAM_DATA_PORT, CAM_LOAD_BIT);
-  cbi(CAM_DATA_PORT, CAM_START_BIT);
-  cbi(CAM_DATA_PORT, CAM_SIN_BIT);
+digitalWrite(CLOCK, LOW);
+digitalWrite(RESET, HIGH);
+digitalWrite(LOAD, LOW);
+digitalWrite(START, LOW);
+digitalWrite(SIN, LOW);
 }
 
 // Sends a 'reset' pulse to the AR chip.
-// START:  XCK Rising Edge
-// FINISH: XCK Just before Falling Edge
+// START:  CLOCK Rising Edge
+// FINISH: CLOCK Just before Falling Edge
 void camReset()
 {
-  camClockH(); // clock high
-  camStepDelay();
-  camStepDelay();
-
-  camClockL(); // clock low
-  camStepDelay();
-  cbi(CAM_DATA_PORT, CAM_RESET_BIT);
-  camStepDelay();
-
-  camClockH(); // clock high
-  camStepDelay();
-  sbi(CAM_DATA_PORT, CAM_RESET_BIT);
-  camStepDelay();
-}
-
-// locally set the value of a register but do not set it in the AR chip. You
-// must run camSendRegisters1 to write the register value in the chip
-void camStoreReg(unsigned char reg, unsigned char data)
-{
-  camReg[reg] = data;
+digitalWrite(CLOCK, HIGH);
+delayMicroseconds(1);
+digitalWrite(CLOCK, LOW);
+delayMicroseconds(1);
+digitalWrite(RESET, LOW);
+delayMicroseconds(1);
+digitalWrite(CLOCK, HIGH);
+delayMicroseconds(1);
+digitalWrite(RESET, HIGH);
+delayMicroseconds(1);
 }
 
 // Reset the camera and set the camera's 8 registers
-// from the locally stored values (see camStoreReg)
+// from the locally stored values 
 void camSetRegisters(void)
 {
   for (reg = 0; reg < 8; ++reg) {
@@ -150,113 +110,99 @@ void camSetRegisters(void)
 }
 
 // Sets one of the 8 8-bit registers in the AR chip.
-// START:  XCK Falling Edge
-// FINISH: XCK Just before Falling Edge
+// START:  CLOCK Falling Edge
+// FINISH: CLOCK Just before Falling Edge
 void camSetReg(unsigned char regaddr, unsigned char regval)
 {
   unsigned char bitmask;
-
   // Write 3-bit address.
   for (bitmask = 0x4; bitmask >= 0x1; bitmask >>= 1) {
-    camClockL();
-    camStepDelay();
-    cbi(CAM_DATA_PORT, CAM_LOAD_BIT);// ensure load bit is cleared from previous call
+    digitalWrite(CLOCK, LOW);
+    delayMicroseconds(1);
+    digitalWrite(LOAD, LOW);// ensure load bit is cleared from previous call
     if (regaddr & bitmask)
-      sbi(CAM_DATA_PORT, CAM_SIN_BIT);// Set the SIN bit
+      digitalWrite(SIN, HIGH);// Set the SIN bit
     else
-      cbi(CAM_DATA_PORT, CAM_SIN_BIT);
-    camStepDelay();
-    camClockH();
-    camStepDelay();
-    cbi(CAM_DATA_PORT, CAM_SIN_BIT);// set the SIN bit low
-    camStepDelay();
+      digitalWrite(SIN, LOW);
+    delayMicroseconds(1);
+    digitalWrite(CLOCK, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(SIN, LOW);// set the SIN bit low
+    delayMicroseconds(1);
   }
 
   // Write 7 most significant bits of 8-bit data.
   for (bitmask = 128; bitmask >= 1; bitmask >>= 1) {
-    camClockL();
-    camStepDelay();
+    digitalWrite(CLOCK, LOW);
+    delayMicroseconds(1);
     if (regval & bitmask)
-      sbi(CAM_DATA_PORT, CAM_SIN_BIT);// set the SIN bit
+      digitalWrite(SIN, HIGH);// set the SIN bit
     else
-      cbi(CAM_DATA_PORT, CAM_SIN_BIT);
-    camStepDelay();
+      digitalWrite(SIN, LOW);
+      delayMicroseconds(1);
     if (bitmask == 1)
-      sbi(CAM_DATA_PORT, CAM_LOAD_BIT);// Assert load at rising edge of xck
-    camClockH();
-    camStepDelay();
-    cbi(CAM_DATA_PORT, CAM_SIN_BIT);// reset the SIN bit
-    camStepDelay();
+      digitalWrite(LOAD, HIGH);// Assert load at rising edge of xck
+    digitalWrite(CLOCK, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(SIN, LOW);
+    delayMicroseconds(1);
   }
+    digitalWrite(LED, HIGH);
+    delay(10);
+    digitalWrite(LED, LOW);
 }
 
-// Take a picture, read it and send it trhough the serial port.
-// getPicture -- send the pixel data to the requester
-// START:  XCK Falling Edge
-// FINISH: XCK Just before Rising Edge
+// Take a picture, read it and send it through the serial port.
+// START:  CLOCK Falling Edge
+// FINISH: CLOCK Just before Rising Edge
 void camReadPicture()
 {
-  boolean searching = true;  // current state
-  boolean bright = false;    // flag for pixel brightness
   unsigned int pixel;       // Buffer for pixel read in
   int x, y;
 
-  // Seems to take a long time to send
-  // Serial.write(CAM_COM_SEND_START);
-
   // Camera START sequence
-  camClockL();
-  camStepDelay();// ensure load bit is cleared from previous call
-  cbi(CAM_DATA_PORT, CAM_LOAD_BIT);// START rises before xck
-  sbi(CAM_DATA_PORT, CAM_START_BIT);
-  camStepDelay();
-  camClockH();
-  camStepDelay();
-  cbi(CAM_DATA_PORT, CAM_START_BIT);// start valid on rising edge of xck, so can drop now
-  camStepDelay();
-  camClockL();
-  camStepDelay();
-  camStepDelay();
+  digitalWrite(CLOCK, LOW);
+  delayMicroseconds(1);// ensure load bit is cleared from previous call
+  digitalWrite(LOAD, LOW);// START rises before CLOCK
+  digitalWrite(START, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(CLOCK, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(START, LOW);// START valid on rising edge of CLOCK, so can drop now
+  delayMicroseconds(1);
+  digitalWrite(CLOCK, LOW);
+  delayMicroseconds(1);
 
   // Wait for READ to go high
   while (1) {
-    camClockH();
-    camStepDelay();
-    // READ goes high with rising XCK
-    //    if ( inp(CAM_READ_PIN) & (1 << CAM_READ_BIT) )
-    if (digitalRead(CAM_READ_PIN) == HIGH) // CAM pin on PB0/D8
+    digitalWrite(CLOCK, HIGH);
+    delayMicroseconds(1);
+    if (digitalRead(READ) == HIGH) // READ goes high with rising CLOCK
       break;
-    camStepDelay();
-    camClockL();
-    camStepDelay();
-    camStepDelay();
+    delayMicroseconds(1);  
+    digitalWrite(CLOCK, LOW);
+    delayMicroseconds(1);  
   }
-  camStepDelay();
+  delayMicroseconds(1);  
 
   for (y = 0; y < 128; y++) {
-    searching = true;                                // start in 'searching' state
     for (x = 0; x < 128; x++) {
-      camClockL();
-      camStepDelay();
-      pixel = analogRead(CAM_ADC_PIN) >> 2;// get the next pixel, buffer it, and send it out over serial
+      digitalWrite(CLOCK, LOW);
+      delayMicroseconds(1);
+      pixel = analogRead(VOUT) >> 2;// get the next pixel, buffer it, and send it out over serial
       Serial.write(pixel);
-      camClockH();
-      camStepDelay();
-      camStepDelay();
-
+      delayMicroseconds(1);  
+      digitalWrite(CLOCK, HIGH);
+      delayMicroseconds(1);  
     } // end for x
   } /* for y */
 
-  // Go through the remaining rows
-  while ( digitalRead(CAM_READ_PIN) == HIGH ) {
-    camClockL();
-    camStepDelay();
-    camStepDelay();
-    camClockH();
-    camStepDelay();
-    camStepDelay();
+  while (digitalRead(READ) == HIGH) { // Go through the remaining rows
+    digitalWrite(CLOCK, LOW);
+    delayMicroseconds(1);  
+    digitalWrite(CLOCK, HIGH);
+    delayMicroseconds(1);  
   }
-  camClockL();
-  camStepDelay();
-  camStepDelay();
+  digitalWrite(CLOCK, LOW);
+  delayMicroseconds(1);  
 }
