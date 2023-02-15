4e0ddd02392c5f32acc41ac9dff849646b77a37d
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2023-02-15 22:27:21 +0100

    Embed autoexposure, remove serial RX/TX, TX only

--- Codes/ArduiCam_Matlab.m
+++ Codes/ArduiCam_Matlab.m
@@ -1,9 +1,8 @@
-% Raphael BOICHOT 13/02/2022
+% Raphael BOICHOT 30/01/2023
 % for any question : raphael.boichot@gmail.com
 
 clear
 clc
-autoexposure='ON' %or OFF
 disp('-----------------------------------------------------')
 disp('|Beware, this code is for Matlab ONLY !!!           |')
 disp('|You can break the code with Ctrl+C on Editor Window|')
@@ -13,119 +12,39 @@ disp('-----------------------------------------------------')
 arduinoObj = serialport("COM4",2000000); %set the Arduino com port here
 %set(arduinoObj, 'timeout',-1);
 flag=0;
-num_image=1;
-error=imread('Error.png');
-mkdir('./images/')
-
-%real settings used by the Game Boy Camera, except exposure
-reg1=0b11100100;%N VH1 VH0 G4 G3 G2 G1 G0
-reg2=0b00000001;%C17 C16 C15 C14 C13 C12 C11 C10 / exposure time by 4096 ms steps (max 1.0486 s)
-reg3=0b00000000;%C07 C06 C05 C04 C03 C02 C01 C00 / exposure time by 16 µs steps (max 4096 ms)
-reg4=0b00000001;%P7 P6 P5 P4 P3 P2 P1 P0 filtering kernels
-reg5=0b00000000;%M7 M6 M5 M4 M3 M2 M1 M0 filtering kernels
-reg6=0b00000001;%X7 X6 X5 X4 X3 X2 X1 X0 filtering kernels
-reg7=0b00000011;%E3 E2 E1 E0 I V2 V1 V0 set Vref
-reg0=0b10100111;%Z1 Z0 O5 O4 O3 O2 O1 O0 zero point calibration and output reference voltage
-
-
+image_counter=0;
+mkdir ./image
 while flag==0 %infinite loop
-    register=[reg0 reg1 reg2 reg3 reg4 reg5 reg6 reg7];%free setting
-    reg2=double(reg2);
-    reg3=double(reg3);
-    data=[];
     data = readline(arduinoObj);
     if not(strlength(data)>100)
         disp(data);
     end
-
-    if not(isempty(strfind(data,'Ready'))); %Inject the registers
-        for k=1:1:8
-            write(arduinoObj,register(k),'uint8'); %send registers to Arduino
-        end
-    end;
+    
     if strlength(data)>1000 %This is an image coming
-        data=double(char(data));
+        data=(char(data));
         offset=1; %First byte is always junk (do not know why, probably a Println LF)
         im=[];
         if length(data)>=16385
             for i=1:1:128 %We get the full image, 5 lines are junk at bottom, top is glitchy due to amplifier artifacts
                 for j=1:1:128
-                    im(i,j)=double(data(offset));
-                    offset=offset+1;
+                    im(i,j)=hex2dec(data(offset:offset+1));
+                    offset=offset+3;
                 end
             end
             raw=im; %We keep the raw data just in case
-            im=im(9:end-8,:); %We get the intersting part of the image, what a Game Boy Camera displays for example
+            maximum=max(max(raw(1:120,:)));
+            minimum=min(min(raw(1:120,:)));
+            moyenne=mean(mean(raw(1:120,:)));
+            figure(1)
+            subplot(1,2,1);
+            histogram(raw(1:120,:),100)
         end
-        
-        if length(data)<16385 % not enough data whatever the reason
-            im=error(:,:,1);
-        end
-
-        %preparing for nice png output with autocontrast
-        minimum=min(min(im));
-        maximum=max(max(im));
-        moyenne=mean(mean(im));
-        disp(['mininum=',num2str(minimum),' maximum=',num2str(maximum), ' moyenne=',num2str(moyenne)])
-        disp(['Number of unique pixel value: ',num2str(length(unique(reshape(im,1,[]))))])
-        im=im-minimum;
-        maximum=max(max(im));
-        slope=255/maximum;
-        im=im*slope;
-        im=im-25;%some contrast adjustement
-        im=im*1.2;%some contrast adjustement
-        im=uint8(im);
-        
-        bayer=Bayer_dithering(im);
-        subplot(2,2,1)
-        imagesc(im)
-        title('Live view 128x112')
-        subplot(2,2,2)
-        imagesc(raw)
-        title('Raw 128x128 image')
-        colormap(gray)
-        subplot(2,2,3)
-        imagesc(bayer)
-        title('Dithered image')
-        subplot(2,2,4)        
-        hist(data,50)
-        title('Pixel histogramm')
-        colormap(gray)
+        image_display=uint8(raw(1:120,:)-minimum)*(255/(maximum-minimum));
+        image_counter=image_counter+1;
+        image_display=imresize(image_display,4,"nearest");
+        imwrite(image_display,['./image/output_',num2str(image_counter),'.gif'],'gif');
+        subplot(1,2,2);
+        imshow(image_display)
         drawnow
-        
-        im=imresize(im,4,'nearest');
-        bayer=imresize(bayer,4,'nearest');
-        imwrite(im,['./images/Arduicam_',num2str(num_image),'.png'])
-        imwrite(bayer,['./images/Arduicam_bayer_',num2str(num_image),'.png'])
-        %end nice png output with autocontrast
-        disp(['Saving Arduicam_',num2str(num_image),'.png'])
-        num_image=num_image+1;
-        
-        if autoexposure=='ON'
-            delta=moyenne-160;
-            disp(['Autoexposure delta = ',num2str(delta)])
-            disp(['Autoexposure rg2 = ',num2str(reg2)])
-            disp(['Autoexposure rg3 = ',num2str(reg3)])
-            disp(['Black pixels level = ',num2str(mean(mean(raw(end-4:end,:))))])
-            %disp(['Exposure time = ',num2str(reg3*16e-6+reg2*255*16e-6),' seconds'])
-            
-            if not(reg2==0); reg3=reg3-min(5*round(delta),255);end;
-            if reg2==0; reg3=reg3-round(0.5*delta);end;
-            
-            if reg3>255;
-                reg3=reg3-255;
-                reg2=reg2+1;
-            end;
-            if reg3<0;
-                reg3=reg3+255;
-                reg2=reg2-1;
-            end;
-            if reg2<0;reg2=0;end;
-            if reg3<0;reg3=0;end;
-            if reg2>255;reg2=255;end;
-            if reg3>255;reg3=255;end;
-            if reg3==0&&reg2==0;reg3=1;end;%minimal exposure time
-
-        end
     end
 end
--- Codes/gbcam_Arduino/gbcam_Arduino.ino
+++ Codes/gbcam_Arduino/gbcam_Arduino.ino
@@ -1,7 +1,7 @@
 //Simplified and completely rewritten by Raphaël BOICHOT in 2022/03/07 to be Arduino/ESP8266/ESP32 compatible
 //from a code of Laurent Saint-Marcel (lstmarcel@yahoo.fr) written in 2005/07/05
 //once cleaned and updated, the code is in fact damn simple to understand
-//the ADC of ESP/Arduino is very slow (successive approximations) compared to Game Boy Camera (Flash ADC).  
+//the ADC of ESP/Arduino is very slow (successive approximations) compared to Game Boy Camera (Flash ADC).
 //ADC slowness is the bottleneck of the code, just reading 128x128 pixels take 1.8 seconds
 //to perform live image rendering, the use of an external flash ADC is probably mandatory or try using https://github.com/avandalen/avdweb_AnalogReadFast
 
@@ -18,9 +18,25 @@ int SIN =   12; //parameter input, pulled down internally
 int START = 13; //Image sensing start, pulled down internally, has to be high before rising CLOCK
 int LED =   4; //just for additionnal LED on D4, not essential to the protocol
 
-unsigned char camReg[8]; //registers
+/*
+  %some real settings used by the Mitsubishi sensor on Game Boy Camera, except exposure
+  reg0=0b10011111;%Z1 Z0 O5 O4 O3 O2 O1 O0 zero point calibration and output reference voltage
+  reg1=0b11101000;%N VH1 VH0 G4 G3 G2 G1 G0
+  reg2=0b00000001;%C17 C16 C15 C14 C13 C12 C11 C10 / exposure time by 4096 ms steps (max 1.0486 s)
+  reg3=0b00000000;%C07 C06 C05 C04 C03 C02 C01 C00 / exposure time by 16 µs steps (max 4096 ms)
+  reg4=0b00000001;%P7 P6 P5 P4 P3 P2 P1 P0 filtering kernels
+  reg5=0b00000000;%M7 M6 M5 M4 M3 M2 M1 M0 filtering kernels
+  reg6=0b00000001;%X7 X6 X5 X4 X3 X2 X1 X0 filtering kernels
+  reg7=0b00000011;%E3 E2 E1 E0 I V2 V1 V0 set Vref
+*/
+//the ADC resolution is 0.8 mV (3.3/2^12, 12 bits) cut to 12.9 mV (8 bits), registers are close of those from the Game Boy Camera in mid light
+//With these registers, the output voltage is between 0.58 and 3.04 volts (on 3.3 volts).
+unsigned char camReg[8] = {0b10011111, 0b11101000, 0b00000001, 0b00000000, 0b00000001, 0b000000000, 0b00000001, 0b00000011}; //registers
+unsigned char v_min = 27; //minimal voltage returned by the sensor in 8 bits DEC (0.58 volts)
+unsigned char v_max = 155;//maximal voltage returned by the sensor in 8 bits DEC (3.04 volts)
 unsigned char reg;
-int cycles=10; //time delay in processor cycles, to fit with your ESP/Arduino frequency, here for Arduino Uno, about 0.62 µs
+unsigned long int accumulator, counter, current_exposure, new_exposure;
+int cycles = 0; //time delay in processor cycles, to fit with your ESP/Arduino frequency, here for Arduino Uno, about 0.62 µs
 
 void setup()
 {
@@ -35,66 +51,88 @@ void setup()
   camInit();
   Serial.println("Version for Arduino with Analog Read Fast");
   Serial.println("Ready for transmission");
-  delay(25);// just to be sure that the registers are sent on Octave side
 }
 
 // there is no special command to trigger sensor, as soon as 8 registers are received, an image is requested to the sensor
-void loop()//the main loop just reads 8 registers from GNU Octave and send an image to serial in 8-bits binary
+void loop()//that's all folks !
 {
-  if (Serial.available() == 8) {// do I have 8 bytes into the read buffer ?
-    digitalWrite(LED, HIGH);
-    for (reg = 0; reg < 8; ++reg) {
-    camReg[reg] = Serial.read(); //read the 8 current registers sent by GNU Octave
-    }
-    delay(25);//the delay here is just to see the LED flashing without slowing down the protocol, can be removed if necessary
-    digitalWrite(LED, LOW);
-
-    Serial.print("Registers injected in sensor: ");
-    for (reg = 0; reg < 8; ++reg) {
-      Serial.print(camReg[reg], DEC);//sent them back to GNU Octave to be sure that everything is OK, may be removed
-      Serial.print(" ");
-    }
-    Serial.println("");
-
-    camReset();
-    camSetRegisters();// Send 8 registers to the sensor
-    camReadPicture(); // get pixels, dump them on serial, GNU Octave reads them and does the rest of the job
-    Serial.println("");
-    Serial.println("Image acquired");
-    camReset();
-    Serial.println("Sensor reset");
-    Serial.println("Ready for transmission");
-    delay(25);// just to be sure that the registers are sent on Octave side
-  }
+  take_a_picture();
+  new_exposure = auto_exposure();// self explanatory
+  push_exposure(); //update exposure registers C2-C3
+  Serial.print("Exposure registers: ");
+  Serial.println(new_exposure, HEX);
 } //end of loop
 
+int auto_exposure() {
+  double exp_regs, new_regs, error, mean_value;
+  unsigned int setpoint = (v_max + v_min) / 2;
+  unsigned char max_line = 120; //last 5-6 rows of pixels contains dark pixel value and various artifacts, so I remove 8 to have a full tile line
+  exp_regs = camReg[2] * 256 + camReg[3];// I know, it's a shame to use a double here but we have plenty of ram
+  mean_value = accumulator / (128 * max_line);
+  error = setpoint - mean_value; // so in case of deviation, registers 2 and 3 are corrected
+  // this part is very similar to what a Game Boy Camera does, except that it does the job with only bitshift operators and in more steps.
+  // Here we can use 32 bits variables for ease of programming.
+  // the bigger the error is, the bigger the correction on exposure is.
+  new_regs = exp_regs;
+  if (error > 80)                     new_regs = exp_regs * 2;
+  if (error < -80)                    new_regs = exp_regs / 2;
+  if ((error <= 80) && (error >= 30))    new_regs = exp_regs * 1.3;
+  if ((error >= -80) && (error <= -30))  new_regs = exp_regs / 1.3;
+  if ((error <= 30) && (error >= 10))     new_regs = exp_regs * 1.03;
+  if ((error >= -30) && (error <= -10))   new_regs = exp_regs / 1.03;
+  if ((error <= 10) && (error >= 4))   new_regs = exp_regs + 1;//this level is critical to avoid flickering in full sun, 3-4 is nice
+  if ((error >= -10) && (error <= -4))  new_regs = exp_regs - 1;//this level is critical to avoid flickering in full sun,  3-4 is nice
+  return int(new_regs);
+}
+
+void push_exposure() {
+  if (new_exposure < 0x0030) new_exposure = 0x0030;//minimum of the sensor for these registers, below there are verticals artifacts, see sensor documentation for details
+  if (new_exposure > 0xFFFF) new_exposure = 0xFFFF;//maximum of the sensor, about 1 second
+  camReg[2] = int(new_exposure / 256);//Janky, I know...
+  camReg[3] = int(new_exposure - camReg[2] * 256);//Janky, I know...
+}
+
+unsigned int get_exposure(unsigned char camReg[8]) {
+  double exp_regs;
+  exp_regs = camReg[2] * 256 + camReg[3];//
+  return exp_regs;
+}
+
+void take_a_picture() {
+  Serial.println("New image:");
+  camReset();// resets the sensor
+  camSetRegisters();// Send 8 registers to the sensor
+  camReadPicture(); // get pixels, dump them in RawCamData
+  camReset();
+  Serial.println("");
+}
 
 void camDelay()// Allow a lag in processor cycles to maintain signals long enough
 {
-for (int i = 0; i < cycles; i++) NOP;
+  for (int i = 0; i < cycles; i++) NOP;
 }
 
 void camInit()// Initialise the IO ports for the camera
 {
-digitalWrite(CLOCK, LOW);
-digitalWrite(RESET, HIGH);
-digitalWrite(LOAD, LOW);
-digitalWrite(START, LOW);
-digitalWrite(SIN, LOW);
+  digitalWrite(CLOCK, LOW);
+  digitalWrite(RESET, HIGH);
+  digitalWrite(LOAD, LOW);
+  digitalWrite(START, LOW);
+  digitalWrite(SIN, LOW);
 }
 
 void camReset()// Sends a RESET pulse to sensor
 {
-digitalWrite(CLOCK, HIGH);
-camDelay();
-digitalWrite(CLOCK, LOW);
-camDelay();
-digitalWrite(RESET, LOW);
-camDelay();
-digitalWrite(CLOCK, HIGH);
-camDelay();
-digitalWrite(RESET, HIGH);
-camDelay();
+  digitalWrite(CLOCK, HIGH);
+  camDelay();
+  digitalWrite(CLOCK, LOW);
+  camDelay();
+  digitalWrite(RESET, LOW);
+  camDelay();
+  digitalWrite(CLOCK, HIGH);
+  camDelay();
+  digitalWrite(RESET, HIGH);
+  camDelay();
 }
 
 void camSetRegisters(void)// Sets the sensor 8 registers
@@ -128,7 +166,7 @@ void camSetReg(unsigned char regaddr, unsigned char regval)// Sets one of the 8 
       digitalWrite(SIN, HIGH);// set the SIN bit
     else
       digitalWrite(SIN, LOW);
-      camDelay();
+    camDelay();
     if (bitmask == 1)
       digitalWrite(LOAD, HIGH);// Assert load at rising edge of CLOCK
     digitalWrite(CLOCK, HIGH);
@@ -154,37 +192,43 @@ void camReadPicture()// Take a picture, read it and send it through the serial p
   camDelay();
   digitalWrite(CLOCK, LOW);
   camDelay();
-  
+  digitalWrite(LED, HIGH);
   while (1) {// Wait for READ to go high
     digitalWrite(CLOCK, HIGH);
     camDelay();
     if (digitalRead(READ) == HIGH) // READ goes high with rising CLOCK
       break;
-    camDelay();  
+    camDelay();
     digitalWrite(CLOCK, LOW);
-    camDelay();  
+    camDelay();
   }
-  camDelay();  
-
+  camDelay();
+  digitalWrite(LED, LOW);
+  accumulator = 0;
+  counter = 0;
   for (y = 0; y < 128; y++) {
     for (x = 0; x < 128; x++) {
       digitalWrite(CLOCK, LOW);
       camDelay();
       //pixel = analogRead(VOUT) >> 2;// The ADC is 10 bits, this sacrifies the 2 least significant bits to simplify transmission
       pixel = analogReadFast(VOUT) >> 2;
-      Serial.write(pixel);
-      camDelay();  
+      //Serial.write(pixel);
+      accumulator = accumulator + pixel;
+      if (pixel <= 0x0F) Serial.print('0');
+      Serial.print(pixel, HEX);
+      Serial.print(" ");
+      camDelay();
       digitalWrite(CLOCK, HIGH);
-      camDelay();  
+      camDelay();
     } // end for x
   } /* for y */
 
   while (digitalRead(READ) == HIGH) { // Go through the remaining rows
     digitalWrite(CLOCK, LOW);
-    camDelay();  
+    camDelay();
     digitalWrite(CLOCK, HIGH);
-    camDelay();  
+    camDelay();
   }
   digitalWrite(CLOCK, LOW);
-  camDelay();  
+  camDelay();
 }
